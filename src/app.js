150d45cbcd1e4b67c8216495cfe6ac83d462009c
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-01-12 19:38:23 +0530

    ip whitelist in cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ import { profileRouter } from "./routes/profile.js";
  import { userRouter } from "./routes/user.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
  app.use(express.json())
  app.use(cookieParser());
  
