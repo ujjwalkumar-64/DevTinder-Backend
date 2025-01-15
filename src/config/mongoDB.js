e9338e40acba4adb8c611ea06d36fe782de6aed9
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-01-16 03:03:21 +0530

    db update

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const mongoDB = async() => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/apiTesting')
+        await mongoose.connect('mongodb+srv://22cs3064:nViEFdRMm3khHWe1@devtinder.3ir8h.mongodb.net/devTinder')
         console.log("db connected")
     } catch (error) {
         console.log("error while db :"+ error)
